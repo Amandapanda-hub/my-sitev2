a0021cd1fd4dedcb6c81ffad550e692dae66dc3f
Amanda Hilton <114438874+Amandapanda-hub@users.noreply.github.com>
2023-08-09 13:32:05 -0600

    adding new links into portfolio

--- src/pages/Portfolio/components/Coffee/Coffee.jsx
+++ src/pages/Portfolio/components/Coffee/Coffee.jsx
@@ -4,9 +4,13 @@ import { AiOutlineLink } from 'react-icons/ai';
 export default function Coffee() {
     const projects = [
         { id: 1, name: 'TechShop', description: 'Fully Responsive, Single-Page, TechShop template.', url: 'https://amandapanda-hub.github.io/techshop/' },
-        { id: 2, name: 'Blissful Blooms', description: 'Fully Responsive, Single-Page, Nature Conservatory template.', url: 'https://blissful-blooms-react-tailwind-55cl.vercel.app/' },        
-        { id: 3, name: 'Elite Elegance', description: 'Fully Responsive, Multi-Page, Nail Salon template.', url: 'https://amandapanda-hub.github.io/Elite-Elegance/' },
-        { id: 4, name: 'Fun Quote Generator', description: 'Fully Responsive, Single-Page, Fun Quote Generator', url: 'https://quote-generator-v1-bdmbggg51-amandapanda-hub.vercel.app/' },
+        { id: 2, name: 'Palette Paradise', description: 'Fully Responsive, Dynamic, Multi-Page, Blog Template', url: 'https://blog-blog-mu.vercel.app/' },
+        { id: 2, name: 'WellVibe', description: 'Fully Responsive, Multi-Page, Business Template with Blog', url: 'https://color-landing-layout.vercel.app/' },
+        { id: 3, name: 'Aquilone', description: 'Single-Section, Responsive Landing Page', url: 'https://basic-box-landingpage-9pyh4cikv-amandapanda-hub.vercel.app/' },
+        { id: 4, name: 'Marine Mirage', description: 'Single-Page, Multi-Section, Responsive Landing Page', url: 'https://underthesea-landingpage.vercel.app/' },
+        { id: 5, name: 'Blissful Blooms', description: 'Fully Responsive, Single-Page, Nature Conservatory template.', url: 'https://blissful-blooms-react-tailwind-55cl.vercel.app/' },        
+        { id: 6, name: 'Elite Elegance', description: 'Fully Responsive, Multi-Page, Nail Salon template.', url: 'https://amandapanda-hub.github.io/Elite-Elegance/' },
+        { id: 7, name: 'Fun Quote Generator', description: 'Fully Responsive, Single-Page, Fun Quote Generator', url: 'https://quote-generator-v1-bdmbggg51-amandapanda-hub.vercel.app/' },
       ];
     
       const handleClick = (e, url) => {
